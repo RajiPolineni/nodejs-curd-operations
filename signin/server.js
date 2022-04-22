6257e82c76917d27780e1b5e3340ff5330af74dd
Rajyalakshmi <rajivenkat@Rajyalakshmis-MacBook-Pro.local>
2022-04-22 10:12:45 +0530

    modified

--- signin/server.js
+++ signin/server.js
@@ -8,8 +8,7 @@ const jwt = require("jsonwebtoken");
 const app = express();
 app.use(express.json());
 let db = null;
-
-initializeDbConnection = async () => {
+const initializeDbConnection = async () => {
   try {
     db = await open({
       filename: path.join(__dirname, "database.db"),
@@ -21,7 +20,6 @@ initializeDbConnection = async () => {
   }
 };
 initializeDbConnection();
-
 app.post("/register/", async (req, res) => {
   const { username, email, password } = req.body;
   const hashedPassword = await bcrypt.hash(password, 10);
@@ -86,9 +84,114 @@ app.get("/users/", (req, res) => {
             SELECT
               *
             FROM
-             users;`;
+             user_list;`;
         const users_list = await db.all(Query);
         res.send(users_list);
+      }
+    });
+  }
+});
+
+//get by userid
+app.get("/get/:userId", async (req, res) => {
+  const { userId } = req.params;
+  let jwtToken;
+  const authHeader = req.headers["authorization"];
+  if (authHeader !== undefined) {
+    jwtToken = authHeader.split(" ")[1];
+  }
+  if (jwtToken === undefined) {
+    res.status(401);
+    res.send("Invalid Access Token");
+  } else {
+    jwt.verify(jwtToken, "krify", async (error, payload) => {
+      if (error) {
+        res.send("Invalid Access Token");
+      } else {
+        const Query = `SELECT * from user_list WHERE id=${userId};`;
+        const users_details = await db.get(Query);
+        res.send(users_details);
+      }
+    });
+  }
+});
+
+//post
+app.post("/post/", async (req, res) => {
+  const { id, name, gender, organization } = req.body;
+  let jwtToken;
+  const authHeader = req.headers["authorization"];
+  if (authHeader !== undefined) {
+    jwtToken = authHeader.split(" ")[1];
+  }
+  if (jwtToken === undefined) {
+    res.status(401);
+    res.send("Invalid Access Token");
+  } else {
+    jwt.verify(jwtToken, "krify", async (error, payload) => {
+      if (error) {
+        res.send("Invalid Access Token");
+      } else {
+        const Query = `INSERT INTO 
+                        user_list(
+                            id,name,gender,organization
+                            )
+                        VALUES(
+                            '${id}','${name}','${gender}','${organization}'
+                            );`;
+        const users_details = await db.run(Query);
+        res.send(users_details);
+      }
+    });
+  }
+});
+
+//put request
+app.put("/put/:userId", async (req, res) => {
+  const { userId } = req.params;
+  const { name } = req.body;
+  let jwtToken;
+  const authHeader = req.headers["authorization"];
+  if (authHeader !== undefined) {
+    jwtToken = authHeader.split(" ")[1];
+  }
+  if (jwtToken === undefined) {
+    res.status(401);
+    res.send("Invalid Access Token");
+  } else {
+    jwt.verify(jwtToken, "krify", async (error, payload) => {
+      if (error) {
+        res.send("Invalid Access Token");
+      } else {
+        const query = `UPDATE user_list
+            SET name='${name}' WHERE id=${userId};`;
+        await db.run(query);
+        res.send("Updated successfully");
+      }
+    });
+  }
+});
+
+//delete request
+app.delete("/delete/:userId", async (req, res) => {
+  const { userId } = req.params;
+  let jwtToken;
+  const authHeader = req.headers["authorization"];
+  if (authHeader !== undefined) {
+    jwtToken = authHeader.split(" ")[1];
+  }
+  if (jwtToken === undefined) {
+    res.status(401);
+    res.send("Invalid Access Token");
+  } else {
+    jwt.verify(jwtToken, "krify", async (error, payload) => {
+      if (error) {
+        res.send("Invalid Access Token");
+      } else {
+        const query = `DELETE FROM user_list 
+            WHERE id=${userId};`;
+        await db.run(query);
+        res.send("deleted successfully");
       }
     });
   }
